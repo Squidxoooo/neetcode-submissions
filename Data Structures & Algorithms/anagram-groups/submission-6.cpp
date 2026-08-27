c80382986d7387e7abfe29f1adeaacfdc9e4790f
Prithvi Singh Kshatriya <squidxo1@gmail.com>
2026-08-28 02:01:52 +0530

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string , vector<string>>grp;
+// sorting idea
+        for(string s : strs) {
+
+             string key = s;
+
+        
+        
+        sort(key.begin() , key.end());
+        grp[key].push_back(s);
+
+        }  
+      vector<vector<string>> ans;
+      for(auto pair : grp) {
+        pair.first;
+        pair.second;
+      
+
+        ans.push_back(pair.second);
+
+      }
+
+        
+    
+      return ans;
+    }
+};
